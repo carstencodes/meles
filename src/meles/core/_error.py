850b5c508a303a1a407aa3b9549729ff96913efe
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2024-07-03 22:28:50 +0200

    feat: Added Error class for handling issues
    The Processing Error is used to transport issues at runtime

---
+++ src/meles/core/_error.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2024 Carsten Igel.
+#
+# This file is part of meles
+# (see https://github.com/carstencodes/meles).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+from http import HTTPStatus
+
+
+class ProcessingError(Exception):
+    status_code: int = HTTPStatus.INTERNAL_SERVER_ERROR
+    message: str = "Internal error"
+
+    def __init__(self, status: int, message: str) -> None:
+        self.status = status
+        self.message = message
