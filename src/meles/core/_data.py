63263117c5968397f398f24a15f395ec21523cb9
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2024-07-03 22:28:50 +0200

    feat: Describe badge data
    Describe how a badge should look like

---
+++ src/meles/core/_data.py
@@ -0,0 +1,35 @@
+#
+# Copyright (c) 2024 Carsten Igel.
+#
+# This file is part of meles
+# (see https://github.com/carstencodes/meles).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:  # pragma: no cover
+    from ._color import Color
+    from ._icons import Icon
+
+
+@dataclass(frozen=True)
+class BadgeData:
+    icon: "Icon | None" = field()
+    label: str = field()
+    text: str = field()
+    color: "Color" = field()
